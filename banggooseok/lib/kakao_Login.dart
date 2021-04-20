e26d326027f7fe01b257b237f4a82f06dedb1a35
davidkim97 <davidmim99@gmail.com>
2021-04-21 00:24:02 +0900

    Flutter-Bang-21

--- banggooseok/lib/kakao_Login.dart
+++ banggooseok/lib/kakao_Login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 import 'package:kakao_flutter_sdk/all.dart';
 import 'package:kakao_flutter_sdk/user.dart';
 
@@ -14,7 +15,6 @@ class KakaoLogin extends StatefulWidget {
 
 class _KakaoLoginState extends State<KakaoLogin> {
   bool _isKakaoTalkInstalled = false;
-  List<Map<String, String>> datas = [];
 
   @override
   void dispose() {
@@ -24,6 +24,122 @@ class _KakaoLoginState extends State<KakaoLogin> {
   @override
   void initState() {
     super.initState();
+    _initKakaoTalkInstalled();
+  }
+
+  _initKakaoTalkInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    print('kakao Install : ' + installed.toString());
+
+    setState(() {
+      _isKakaoTalkInstalled = installed;
+    });
+  }
+
+  _issueAccessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      print(token);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginDone()),
+      );
+    } catch (e) {
+      print("error on issuing access token: $e");
+    }
+  }
+
+  _loginWithKakao() async {
+    try {
+      var code = await AuthCodeClient.instance.request();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _loginWithTalk() async {
+    try {
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  logOutTalk() async {
+    try {
+      var code = await UserApi.instance.logout();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  unlinkTalk() async {
+    try {
+      var code = await UserApi.instance.unlink();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    isKakaoTalkInstalled();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("방구석"),
+        elevation: 0.0,
+        actions: [],
+      ),
+      body: Center(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 300.0,
+                    height: 50.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: RaisedButton(
+                        child: Text(
+                          "카카오계정 로그인",
+                        ),
+                        color: Colors.yellow,
+                        onPressed: _isKakaoTalkInstalled
+                            ? _loginWithTalk
+                            : _loginWithKakao),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginDone extends StatefulWidget {
+  @override
+  _LoginDoneState createState() => _LoginDoneState();
+}
+
+class _LoginDoneState extends State<LoginDone> {
+  List<Map<String, String>> datas = [];
+  int _currentPageIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPageIndex = 0;
     datas = [
       {
         "cid": "1",
@@ -106,111 +222,8 @@ class _KakaoLoginState extends State<KakaoLogin> {
         "likes": "7"
       },
     ];
-    _initKakaoTalkInstalled();
   }
 
-  _initKakaoTalkInstalled() async {
-    final installed = await isKakaoTalkInstalled();
-    print('kakao Install : ' + installed.toString());
-
-    setState(() {
-      _isKakaoTalkInstalled = installed;
-    });
-  }
-
-  _issueAccessToken(String authCode) async {
-    try {
-      var token = await AuthApi.instance.issueAccessToken(authCode);
-      AccessTokenStore.instance.toStore(token);
-      print(token);
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => LoginDone()),
-      );
-    } catch (e) {
-      print("error on issuing access token: $e");
-    }
-  }
-
-  _loginWithKakao() async {
-    try {
-      var code = await AuthCodeClient.instance.request();
-      await _issueAccessToken(code);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  _loginWithTalk() async {
-    try {
-      var code = await AuthCodeClient.instance.requestWithTalk();
-      await _issueAccessToken(code);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  logOutTalk() async {
-    try {
-      var code = await UserApi.instance.logout();
-      print(code.toString());
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  unlinkTalk() async {
-    try {
-      var code = await UserApi.instance.unlink();
-      print(code.toString());
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    isKakaoTalkInstalled();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("방구석"),
-        elevation: 0.0,
-        actions: [],
-      ),
-      body: Center(
-        child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ButtonTheme(
-                    minWidth: 300.0,
-                    height: 50.0,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    child: RaisedButton(
-                        child: Text(
-                          "카카오계정 로그인",
-                        ),
-                        color: Colors.yellow,
-                        onPressed: _isKakaoTalkInstalled
-                            ? _loginWithTalk
-                            : _loginWithKakao),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LoginDone extends StatelessWidget {
   Future<bool> _getUser() async {
     try {
       User user = await UserApi.instance.me();
@@ -245,13 +258,75 @@ class LoginDone extends StatelessWidget {
     );
   }
 
+  final oCcy = new NumberFormat("#,###", "ko_KR");
+  String calcStringToWon(String priceString) {
+    return "${oCcy.format(int.parse(priceString))}원";
+  }
+
   Widget _bodyWidget() {
     return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       itemBuilder: (BuildContext _context, int index) {
         return Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
           child: Row(
             children: [
-              Image.asset(datas[index]["image"]),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: Image.asset(
+                  datas[index]["image"],
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        datas[index]["title"],
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        datas[index]["location"],
+                        style: TextStyle(
+                            fontSize: 12, color: Colors.black.withOpacity(0.3)),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        calcStringToWon(datas[index]["price"]),
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/heart_off.svg",
+                              width: 13,
+                              height: 13,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(datas[index]["likes"]),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         );
@@ -266,12 +341,47 @@ class LoginDone extends StatelessWidget {
     );
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(
+      String iconName, String label) {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: SvgPicture.asset("assets/svg/${iconName}_off.svg", width: 22),
+      ),
+      label: label,
+    );
+  }
+
+  Widget _bootomNavigationBarwidget() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) {
+        print(index);
+        setState(() {
+          _currentPageIndex = index;
+        });
+      },
+      selectedFontSize: 12,
+      currentIndex: _currentPageIndex,
+      selectedItemColor: Colors.black,
+      selectedLabelStyle: TextStyle(color: Colors.black),
+      items: [
+        _bottomNavigationBarItem("home", "홈"),
+        _bottomNavigationBarItem("notes", "매물등록"),
+        _bottomNavigationBarItem("location", "내 근처"),
+        _bottomNavigationBarItem("chat", "채팅"),
+        _bottomNavigationBarItem("user", "내정보"),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     _getUser();
     return Scaffold(
       appBar: _appbarWidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bootomNavigationBarwidget(),
     );
   }
 }
