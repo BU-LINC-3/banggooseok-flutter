48c89f35f9338e1089f01c683eba8dcfbd859260
davidkim97 <davidmim99@gmail.com>
2021-04-20 18:31:45 +0900

    Banggooseok

---
+++ banggooseok/lib/kakao_Login.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:kakao_flutter_sdk/all.dart';
+import 'package:kakao_flutter_sdk/user.dart';
+
+class KakaoLogin extends StatefulWidget {
+  KakaoLogin({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _KakaoLoginState createState() => _KakaoLoginState();
+}
+
+class _KakaoLoginState extends State<KakaoLogin> {
+  bool _isKakaoTalkInstalled = false;
+  List<Map<String, String>> datas = [];
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    datas = [
+      {
+        "cid": "1",
+        "image": "assets/images/ara-1.jpg",
+        "title": "네메시스 축구화275",
+        "location": "제주 제주시 아라동",
+        "price": "30000",
+        "likes": "2"
+      },
+      {
+        "cid": "2",
+        "image": "assets/images/ara-2.jpg",
+        "title": "LA갈비 5kg팔아요~",
+        "location": "제주 제주시 아라동",
+        "price": "100000",
+        "likes": "5"
+      },
+      {
+        "cid": "3",
+        "image": "assets/images/ara-3.jpg",
+        "title": "치약팝니다",
+        "location": "제주 제주시 아라동",
+        "price": "5000",
+        "likes": "0"
+      },
+      {
+        "cid": "4",
+        "image": "assets/images/ara-4.jpg",
+        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
+        "location": "제주 제주시 아라동",
+        "price": "2500000",
+        "likes": "6"
+      },
+      {
+        "cid": "5",
+        "image": "assets/images/ara-5.jpg",
+        "title": "디월트존기임팩",
+        "location": "제주 제주시 아라동",
+        "price": "150000",
+        "likes": "2"
+      },
+      {
+        "cid": "6",
+        "image": "assets/images/ara-6.jpg",
+        "title": "갤럭시s10",
+        "location": "제주 제주시 아라동",
+        "price": "180000",
+        "likes": "2"
+      },
+      {
+        "cid": "7",
+        "image": "assets/images/ara-7.jpg",
+        "title": "선반",
+        "location": "제주 제주시 아라동",
+        "price": "15000",
+        "likes": "2"
+      },
+      {
+        "cid": "8",
+        "image": "assets/images/ara-8.jpg",
+        "title": "냉장 쇼케이스",
+        "location": "제주 제주시 아라동",
+        "price": "80000",
+        "likes": "3"
+      },
+      {
+        "cid": "9",
+        "image": "assets/images/ara-9.jpg",
+        "title": "대우 미니냉장고",
+        "location": "제주 제주시 아라동",
+        "price": "30000",
+        "likes": "3"
+      },
+      {
+        "cid": "10",
+        "image": "assets/images/ara-10.jpg",
+        "title": "멜킨스 풀업 턱걸이 판매합니다.",
+        "location": "제주 제주시 아라동",
+        "price": "50000",
+        "likes": "7"
+      },
+    ];
+    _initKakaoTalkInstalled();
+  }
+
+  _initKakaoTalkInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    print('kakao Install : ' + installed.toString());
+
+    setState(() {
+      _isKakaoTalkInstalled = installed;
+    });
+  }
+
+  _issueAccessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      print(token);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LoginDone()),
+      );
+    } catch (e) {
+      print("error on issuing access token: $e");
+    }
+  }
+
+  _loginWithKakao() async {
+    try {
+      var code = await AuthCodeClient.instance.request();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _loginWithTalk() async {
+    try {
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  logOutTalk() async {
+    try {
+      var code = await UserApi.instance.logout();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  unlinkTalk() async {
+    try {
+      var code = await UserApi.instance.unlink();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    isKakaoTalkInstalled();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("방구석"),
+        elevation: 0.0,
+        actions: [],
+      ),
+      body: Center(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 300.0,
+                    height: 50.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: RaisedButton(
+                        child: Text(
+                          "카카오계정 로그인",
+                        ),
+                        color: Colors.yellow,
+                        onPressed: _isKakaoTalkInstalled
+                            ? _loginWithTalk
+                            : _loginWithKakao),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginDone extends StatelessWidget {
+  Future<bool> _getUser() async {
+    try {
+      User user = await UserApi.instance.me();
+      print(user.toString());
+    } on KakaoAuthException catch (e) {} catch (e) {}
+  }
+
+  Widget _appbarWidget() {
+    return AppBar(
+      title: GestureDetector(
+        onTap: () {
+          print("click");
+        },
+        child: Row(
+          children: [
+            Text("방구석"),
+            Icon(Icons.arrow_drop_down),
+          ],
+        ),
+      ),
+      elevation: 0.0,
+      actions: [
+        IconButton(icon: Icon(Icons.search), onPressed: () {}),
+        IconButton(icon: Icon(Icons.tune), onPressed: () {}),
+        IconButton(
+            icon: SvgPicture.asset(
+              "assets/svg/bell.svg",
+              width: 22,
+            ),
+            onPressed: () {}),
+      ],
+    );
+  }
+
+  Widget _bodyWidget() {
+    return ListView.separated(
+      itemBuilder: (BuildContext _context, int index) {
+        return Container(
+          child: Row(
+            children: [
+              Image.asset(datas[index]["image"]),
+            ],
+          ),
+        );
+      },
+      itemCount: 10,
+      separatorBuilder: (BuildContext _context, int index) {
+        return Container(
+          height: 1,
+          color: Colors.black.withOpacity(0.4),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getUser();
+    return Scaffold(
+      appBar: _appbarWidget(),
+      body: _bodyWidget(),
+    );
+  }
+}
---
+++ banggooseok/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:banggooseok/kakao_Login.dart';
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/all.dart';
+import 'package:kakao_flutter_sdk/user.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    KakaoContext.clientId = "666d81e2b81db2b86ce399034e19bf47";
+    KakaoContext.javascriptClientId = "9ae9abd30eee57a7959e6abe3d148b81";
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "방구석",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+      ),
+      home: KakaoLogin(),
+    );
+  }
+}
